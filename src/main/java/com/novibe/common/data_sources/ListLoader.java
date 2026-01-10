f6224b34e37863aa1d7bfc42df89b31a55ce4392
Vasiliy Baranovsky <remindscope@gmail.com>
2026-01-10 22:29:09 +0300

    fixed exception for NextDNS while operating block list

--- src/main/java/com/novibe/common/data_sources/ListLoader.java
+++ src/main/java/com/novibe/common/data_sources/ListLoader.java
@@ -14,6 +14,7 @@ import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.StructuredTaskScope;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Profile("CLOUDFLARE")
@@ -39,7 +40,7 @@ public abstract class ListLoader<T> {
                 .map(StructuredTaskScope.Subtask::get)
                 .flatMap(this::lineParser)
                 .distinct()
-                .toList();
+                .collect(Collectors.toCollection(ArrayList::new));
     }
 
     @SneakyThrows
